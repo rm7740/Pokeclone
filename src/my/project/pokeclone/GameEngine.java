8abbdea480b292860b71f22808260c7313683ec1
rm7740 <adavid8a@hotmail.com>
2016-02-15 16:32:44 -0500

    Add start, stop & run methods on GameEngine

--- src/my/project/pokeclone/GameEngine.java
+++ src/my/project/pokeclone/GameEngine.java
@@ -1,7 +1,69 @@
 package my.project.pokeclone;
 
-/**
- * Created by Dell on 15/2/2016.
- */
-public class GameEngine {
+public class GameEngine implements Runnable{
+
+    private boolean isRunning = false;
+    private Thread thread;
+
+    public void start() {
+        if (isRunning) return;
+        isRunning = true;
+
+        thread = new Thread(this);
+        thread.run();
+    }
+
+    public void stop() {
+        if (!isRunning) return;
+        isRunning = false;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        init();
+
+        int fps = 60;
+        double timePerUpdate = 1000000000 / fps;
+        double delta = 0;
+        long now;
+        long lastTime = System.nanoTime();
+        long timer = 0;
+        int updates = 0;
+
+        while(isRunning) {
+            now = System.nanoTime();
+            delta += (now - lastTime) / timePerUpdate;
+            timer += (now - lastTime);
+            lastTime = now;
+            if (delta >= 1) {
+                update();
+                render();
+                updates++;
+                delta--;
+            }
+            if(timer >= 1000000000){
+                System.out.println("Updates and frames: " + updates);
+                timer = 0;
+                updates = 0;
+            }
+        }
+        stop();
+    }
+
+    private void render() {
+
+    }
+
+    private void update() {
+
+    }
+
+    private void init() {
+    }
 }
