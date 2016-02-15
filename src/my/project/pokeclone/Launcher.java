2fdd7fca3576d01f0d23ecd0f96b3e99ed9f6e96
rm7740 <adavid8a@hotmail.com>
2016-02-15 17:13:05 -0500

    Fix Launcher

--- src/my/project/pokeclone/Launcher.java
+++ src/my/project/pokeclone/Launcher.java
@@ -2,7 +2,7 @@ package my.project.pokeclone;
 
 public class Launcher {
     public static void main(String[] args) {
-        GameEngine gameEngine = new GameEngine();
+        GameEngine gameEngine = new GameEngine("Pokeclone!", 240, 160);
         gameEngine.start();
     }
 }
