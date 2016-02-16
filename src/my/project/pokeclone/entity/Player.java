743a26582decbb92ba23fc015df3b95b221689f3
rm7740 <adavid8a@hotmail.com>
2016-02-16 17:23:05 -0500

    Add Player & getInput method, fixed update & render

---
+++ src/my/project/pokeclone/entity/Player.java
@@ -0,0 +1,38 @@
+package my.project.pokeclone.entity;
+
+import my.project.pokeclone.Handler;
+import my.project.pokeclone.graphics.Asset;
+
+import java.awt.*;
+
+public class Player extends LiveEntity{
+
+    public Player(Handler handler, int x, int y) {
+        super(handler, x, y);
+        width = 16;
+        height = 32;
+        speed = 1;
+
+        bounds = new Rectangle(x, y +32, width, height - 32);
+    }
+
+    public void getInput() {
+        yMove = 0;
+        xMove = 0;
+        if (handler.getKeyManager().up) yMove = -speed;
+        if (handler.getKeyManager().down) yMove = speed;
+        if (handler.getKeyManager().left) xMove = -speed;
+        if (handler.getKeyManager().right) xMove = speed;
+    }
+
+    @Override
+    public void update() {
+        getInput();
+        move();
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+        graphics.drawImage(Asset.heroRight, x, y, null, null);
+    }
+}
