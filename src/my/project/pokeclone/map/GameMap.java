b23f7e367da220379b4f3468ab5bd2e05d52990b
rm7740 <adavid8a@hotmail.com>
2016-02-16 15:02:04 -0500

    Make GameMap abstract & fix render to use an offset

--- src/my/project/pokeclone/map/GameMap.java
+++ src/my/project/pokeclone/map/GameMap.java
@@ -1,23 +1,28 @@
 package my.project.pokeclone.map;
 
 import my.project.pokeclone.Handler;
+import my.project.pokeclone.entity.EntityManager;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class GameMap {
+public abstract class GameMap {
 
-    private Handler handler;
-    private int width, height;
-    private BufferedImage bufferedImage;
+    protected Handler handler;
+    protected int width, height;
+    protected BufferedImage bufferedImage;
+    protected int xOffset, yOffset;
+    protected EntityManager entityManager;
 
     public GameMap(Handler handler, BufferedImage bufferedImage) {
         this.handler = handler;
         this.bufferedImage = bufferedImage;
+
+        entityManager = new EntityManager(handler);
     }
 
     public void update() {}
 
     public void render(Graphics graphics) {
-        graphics.drawImage(bufferedImage, 0, 0, null, null);
+        graphics.drawImage(bufferedImage, xOffset, yOffset, null, null);
     }
 }
