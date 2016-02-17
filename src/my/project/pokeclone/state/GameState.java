fb5f5a0aab6629d017522e4c67944cb31aebf1f2
rm7740 <adavid8a@hotmail.com>
2016-02-16 22:27:58 -0500

    Fix the constructor & loadMaps to pass the player to the map

--- src/my/project/pokeclone/state/GameState.java
+++ src/my/project/pokeclone/state/GameState.java
@@ -1,7 +1,7 @@
 package my.project.pokeclone.state;
 
 import my.project.pokeclone.Handler;
-import my.project.pokeclone.graphics.Asset;
+import my.project.pokeclone.entity.Player;
 import my.project.pokeclone.map.GameMap;
 import my.project.pokeclone.map.PalletTown;
 
@@ -11,17 +11,20 @@ public class GameState extends State{
 
     private GameMap palletTown;
     private GameMap currentMap;
+    private Player player;
 
     public GameState(Handler handler) {
         super(handler);
+        player = new Player(handler, 200, 32);
 
         loadMaps();
         currentMap = palletTown;
         handler.setGameMap(currentMap);
+
     }
 
     private void loadMaps() {
-        palletTown = new PalletTown(handler, Asset.palletTown);
+        palletTown = new PalletTown(handler, player);
     }
 
     @Override
