ab81c3e6930865b22b26da1b8f0ccc67500b3d42
rm7740 <adavid8a@hotmail.com>
2016-02-17 00:18:44 -0500

    Add getIntoMapLimits on GameCamera

--- src/my/project/pokeclone/display/GameCamera.java
+++ src/my/project/pokeclone/display/GameCamera.java
@@ -16,6 +16,20 @@ public class GameCamera {
     public void centerOnEntity(Entity entity) {
         xOffset = entity.getX() + (entity.getWidth() / 2) - (handler.getGameWidth() / 2);
         yOffset = entity.getY() + (entity.getHeight() / 2) -(handler.getGameHeight() / 2);
+
+        getIntoMapLimits();
+    }
+
+    public void getIntoMapLimits() {
+        if (xOffset < 0) xOffset = 0;
+        if (xOffset > (handler.getGameMap().getWidth() - handler.getGameWidth())) {
+            xOffset = (handler.getGameMap().getWidth() - handler.getGameWidth());
+        }
+
+        if (yOffset < 0) yOffset = 0;
+        if (yOffset > (handler.getGameMap().getHeight() - handler.getGameHeight())) {
+            yOffset = (handler.getGameMap().getHeight() - handler.getGameHeight());
+        }
     }
 
     public int getxOffset() {
