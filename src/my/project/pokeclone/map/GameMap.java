807d907505a959751b1e96134f0c1facd527f188
rm7740 <adavid8a@hotmail.com>
2016-02-16 22:26:23 -0500

    Fix the constructor, update & render on GameMap

--- src/my/project/pokeclone/map/GameMap.java
+++ src/my/project/pokeclone/map/GameMap.java
@@ -2,6 +2,7 @@ package my.project.pokeclone.map;
 
 import my.project.pokeclone.Handler;
 import my.project.pokeclone.entity.EntityManager;
+import my.project.pokeclone.entity.Player;
 import my.project.pokeclone.util.TextFileLoader;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -16,19 +17,24 @@ public abstract class GameMap {
     protected int[][] collisionTiles;
     protected int horizontalTiles, verticalTiles;
     protected int tileSide;
+    protected Player player;
 
-    public GameMap(Handler handler) {
+    public GameMap(Handler handler, Player player) {
         this.handler = handler;
+        this.player = player;
         entityManager = new EntityManager(handler);
         init();
     }
 
     protected abstract void init();
 
-    public void update() {}
+    public void update() {
+        entityManager.update();
+    }
 
     public void render(Graphics graphics) {
         graphics.drawImage(bufferedImage, xOffset, yOffset, null, null);
+        entityManager.render(graphics);
     }
 
     public void loadCollisionTiles(String path) {
