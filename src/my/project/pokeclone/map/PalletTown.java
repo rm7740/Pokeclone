ed44094440cbe506b1b2c0b6d64eb0d30fb0c192
rm7740 <adavid8a@hotmail.com>
2016-02-16 22:25:22 -0500

    Add a Player to entityManager on PalletTown

--- src/my/project/pokeclone/map/PalletTown.java
+++ src/my/project/pokeclone/map/PalletTown.java
@@ -1,12 +1,13 @@
 package my.project.pokeclone.map;
 
 import my.project.pokeclone.Handler;
+import my.project.pokeclone.entity.Player;
 import my.project.pokeclone.graphics.Asset;
 
 public class PalletTown extends GameMap{
 
-    public PalletTown(Handler handler) {
-        super(handler);
+    public PalletTown(Handler handler, Player player) {
+        super(handler, player);
     }
 
     @Override
@@ -15,7 +16,8 @@ public class PalletTown extends GameMap{
         width = Asset.palletTown.getWidth();
         height = Asset.palletTown.getHeight();
         tileSide = 16;
-
         loadCollisionTiles("res/map tiles/Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town - Tile Map.txt");
+
+        entityManager.addEntity(player);
     }
 }
