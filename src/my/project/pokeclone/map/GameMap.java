22f9ef3fc37a9a50737bedd64bae80665885abaa
rm7740 <adavid8a@hotmail.com>
2016-02-16 22:03:30 -0500

    Add loadCollisionTile & getCollisionTile on GameMap

--- src/my/project/pokeclone/map/GameMap.java
+++ src/my/project/pokeclone/map/GameMap.java
@@ -2,6 +2,7 @@ package my.project.pokeclone.map;
 
 import my.project.pokeclone.Handler;
 import my.project.pokeclone.entity.EntityManager;
+import my.project.pokeclone.util.TextFileLoader;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -12,17 +13,40 @@ public abstract class GameMap {
     protected BufferedImage bufferedImage;
     protected int xOffset, yOffset;
     protected EntityManager entityManager;
+    protected int[][] collisionTiles;
+    protected int horizontalTiles, verticalTiles;
+    protected int tileSide;
 
-    public GameMap(Handler handler, BufferedImage bufferedImage) {
+    public GameMap(Handler handler) {
         this.handler = handler;
-        this.bufferedImage = bufferedImage;
-
         entityManager = new EntityManager(handler);
+        init();
     }
+
+    protected abstract void init();
 
     public void update() {}
 
     public void render(Graphics graphics) {
         graphics.drawImage(bufferedImage, xOffset, yOffset, null, null);
+    }
+
+    public void loadCollisionTiles(String path) {
+        String mapFile = TextFileLoader.loadFileAsString(path);
+        String[] tokens = mapFile.split("\\s+");
+
+        horizontalTiles = width / tileSide;
+        verticalTiles = height / tileSide;
+
+        collisionTiles = new int[horizontalTiles][verticalTiles];
+        for(int y = 0; y < verticalTiles; y++){
+            for(int x = 0; x < horizontalTiles; x++){
+                collisionTiles[x][y] = Integer.parseInt(tokens[x + (y * horizontalTiles)]);
+            }
+        }
+    }
+
+    public int getCollisionTile(int x, int y) {
+        return collisionTiles[x][y];
     }
 }
