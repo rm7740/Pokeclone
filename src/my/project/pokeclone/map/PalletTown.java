33b8379e73090b5c2cae35d6224b1ab841c3c471
rm7740 <adavid8a@hotmail.com>
2016-02-16 22:05:20 -0500

    Make PalletTown work properly

--- src/my/project/pokeclone/map/PalletTown.java
+++ src/my/project/pokeclone/map/PalletTown.java
@@ -1,13 +1,21 @@
 package my.project.pokeclone.map;
 
 import my.project.pokeclone.Handler;
-import java.awt.image.BufferedImage;
+import my.project.pokeclone.graphics.Asset;
 
 public class PalletTown extends GameMap{
 
-    public PalletTown(Handler handler, BufferedImage bufferedImage) {
-        super(handler, bufferedImage);
-        width = 384;
-        height = 320;
+    public PalletTown(Handler handler) {
+        super(handler);
+    }
+
+    @Override
+    protected void init() {
+        bufferedImage = Asset.palletTown;
+        width = Asset.palletTown.getWidth();
+        height = Asset.palletTown.getHeight();
+        tileSide = 16;
+
+        loadCollisionTiles("res/map tiles/Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town - Tile Map.txt");
     }
 }
