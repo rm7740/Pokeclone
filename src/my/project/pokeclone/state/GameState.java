8aa012568ce002cf82cc25f5344d25ac2d342a43
rm7740 <adavid8a@hotmail.com>
2016-02-25 17:02:06 -0500

    Add new maps to the GameState

--- src/my/project/pokeclone/state/GameState.java
+++ src/my/project/pokeclone/state/GameState.java
@@ -2,18 +2,17 @@ package my.project.pokeclone.state;
 
 import my.project.pokeclone.Handler;
 import my.project.pokeclone.entity.Player;
-import my.project.pokeclone.map.GameMap;
-import my.project.pokeclone.map.HeroHome1F;
-import my.project.pokeclone.map.HeroHome2F;
-import my.project.pokeclone.map.PalletTown;
+import my.project.pokeclone.map.*;
 
 import java.awt.*;
 
 public class GameState extends State{
 
-    private GameMap palletTown;
-    private GameMap heroHome1F;
     private GameMap heroHome2F;
+    private GameMap heroHome1F;
+    private GameMap palletTown;
+    private GameMap rivalHome1F;
+    private GameMap oaksLab1F;
     private Player player;
 
     public GameState(Handler handler) {
@@ -29,6 +28,8 @@ public class GameState extends State{
         heroHome2F = new HeroHome2F(handler, player);
         heroHome1F = new HeroHome1F(handler, player);
         palletTown = new PalletTown(handler, player);
+        rivalHome1F = new RivalHome1F(handler, player);
+        oaksLab1F = new OaksLab1F(handler, player);
     }
 
     @Override
@@ -51,5 +52,13 @@ public class GameState extends State{
 
     public GameMap getHeroHome2F() {
         return heroHome2F;
+    }
+
+    public GameMap getRivalHome1F() {
+        return rivalHome1F;
+    }
+
+    public GameMap getOaksLab1F() {
+        return oaksLab1F;
     }
 }
