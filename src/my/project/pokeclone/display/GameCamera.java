1141dd74f079a3ebefd4bd978f0f6958c306806b
rm7740 <adavid8a@hotmail.com>
2016-02-24 22:33:50 -0500

    Add centerSmallMap on GameCamera

--- src/my/project/pokeclone/display/GameCamera.java
+++ src/my/project/pokeclone/display/GameCamera.java
@@ -16,19 +16,30 @@ public class GameCamera {
     public void centerOnEntity(Entity entity) {
         xOffset = entity.getX() + (entity.getWidth() / 2) - (handler.getGameWidth() / 2);
         yOffset = entity.getY() + (entity.getHeight() / 2) -(handler.getGameHeight() / 2);
-
         getIntoMapLimits();
+        centerSmallMap();
     }
 
     public void getIntoMapLimits() {
         if (xOffset < 0) xOffset = 0;
-        if (xOffset > (handler.getGameMap().getWidth() - handler.getGameWidth())) {
+        else if (xOffset > (handler.getGameMap().getWidth() - handler.getGameWidth())) {
             xOffset = (handler.getGameMap().getWidth() - handler.getGameWidth());
         }
 
         if (yOffset < 0) yOffset = 0;
-        if (yOffset > (handler.getGameMap().getHeight() - handler.getGameHeight())) {
+        else if (yOffset > (handler.getGameMap().getHeight() - handler.getGameHeight())) {
             yOffset = (handler.getGameMap().getHeight() - handler.getGameHeight());
+        }
+    }
+
+    public void centerSmallMap() {
+        if (handler.getGameMap().getHeight() < handler.getGameHeight()) {
+            int heightDiff = handler.getGameHeight() - handler.getGameMap().getHeight();
+            yOffset = -heightDiff / 2;
+        }
+        if (handler.getGameMap().getWidth() < handler.getGameWidth()) {
+            int widthDiff = handler.getGameWidth() - handler.getGameMap().getWidth();
+            xOffset = -widthDiff / 2;
         }
     }
 
