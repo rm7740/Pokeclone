ea57b7020c055432ff359a528788c0c5edea2fd1
rm7740 <adavid8a@hotmail.com>
2016-02-24 21:41:26 -0500

    Add getDestination(Map&Position) on PalletTown

--- src/my/project/pokeclone/map/PalletTown.java
+++ src/my/project/pokeclone/map/PalletTown.java
@@ -16,8 +16,27 @@ public class PalletTown extends GameMap{
         width = Asset.palletTown.getWidth();
         height = Asset.palletTown.getHeight();
         tileSide = 16;
+        super.init();
         loadCollisionTiles("res/map tiles/Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town - Tile Map.txt");
+        loadTeleportTiles("res/map tiles/Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town - Teleport Map.txt");
 
         entityManager.addEntity(player);
+    }
+
+    @Override
+    public GameMap getDestinationMap(int marker) {
+        GameMap destination = this;
+        if (marker == 1) destination = handler.getGameState().getHeroHome1F();
+        return destination;
+    }
+
+    @Override
+    public int[] getDestinationPosition(int marker) {
+        int[] position = {0, 0};
+        if (marker == 1) {
+            position[0] = 48;
+            position[1] = 111;
+        }
+        return position;
     }
 }
