297f7ac4dd8837fe73b3330bd908ce11d3bdb6d9
rm7740 <adavid8a@hotmail.com>
2016-02-24 21:43:07 -0500

    Add another GameMap on GameState & some getters

--- src/my/project/pokeclone/state/GameState.java
+++ src/my/project/pokeclone/state/GameState.java
@@ -3,6 +3,7 @@ package my.project.pokeclone.state;
 import my.project.pokeclone.Handler;
 import my.project.pokeclone.entity.Player;
 import my.project.pokeclone.map.GameMap;
+import my.project.pokeclone.map.HeroHome1F;
 import my.project.pokeclone.map.PalletTown;
 
 import java.awt.*;
@@ -10,7 +11,7 @@ import java.awt.*;
 public class GameState extends State{
 
     private GameMap palletTown;
-    private GameMap currentMap;
+    private GameMap heroHome1F;
     private Player player;
 
     public GameState(Handler handler) {
@@ -18,26 +19,30 @@ public class GameState extends State{
         player = new Player(handler, 192, 32, true);
 
         loadMaps();
-        currentMap = palletTown;
-        handler.setGameMap(currentMap);
+        handler.setGameMap(palletTown);
 
     }
 
     private void loadMaps() {
         palletTown = new PalletTown(handler, player);
+        heroHome1F = new HeroHome1F(handler, player);
     }
 
     @Override
     public void update() {
-        currentMap.update();
+        handler.getGameMap().update();
     }
 
     @Override
     public void render(Graphics graphics) {
-        currentMap.render(graphics);
+        handler.getGameMap().render(graphics);
     }
 
-    public void setCurrentMap(GameMap currentMap) {
-        this.currentMap = currentMap;
+    public GameMap getPalletTown() {
+        return palletTown;
+    }
+
+    public GameMap getHeroHome1F() {
+        return heroHome1F;
     }
 }
