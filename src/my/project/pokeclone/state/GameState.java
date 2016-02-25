5f97198be0154da9584c9ba62e9089c1f2bfa644
rm7740 <adavid8a@hotmail.com>
2016-02-25 15:09:05 -0500

    Add the heroHome2F to the GameState

--- src/my/project/pokeclone/state/GameState.java
+++ src/my/project/pokeclone/state/GameState.java
@@ -4,6 +4,7 @@ import my.project.pokeclone.Handler;
 import my.project.pokeclone.entity.Player;
 import my.project.pokeclone.map.GameMap;
 import my.project.pokeclone.map.HeroHome1F;
+import my.project.pokeclone.map.HeroHome2F;
 import my.project.pokeclone.map.PalletTown;
 
 import java.awt.*;
@@ -12,6 +13,7 @@ public class GameState extends State{
 
     private GameMap palletTown;
     private GameMap heroHome1F;
+    private GameMap heroHome2F;
     private Player player;
 
     public GameState(Handler handler) {
@@ -24,8 +26,9 @@ public class GameState extends State{
     }
 
     private void loadMaps() {
-        palletTown = new PalletTown(handler, player);
+        heroHome2F = new HeroHome2F(handler, player);
         heroHome1F = new HeroHome1F(handler, player);
+        palletTown = new PalletTown(handler, player);
     }
 
     @Override
@@ -44,5 +47,9 @@ public class GameState extends State{
 
     public GameMap getHeroHome1F() {
         return heroHome1F;
+    }
+
+    public GameMap getHeroHome2F() {
+        return heroHome2F;
     }
 }
