41b7a5b61b0cefee8b15c3b45b4b1bf04371eeb0
rm7740 <adavid8a@hotmail.com>
2016-02-24 21:42:13 -0500

    Add HeroHome1F & it's methods

---
+++ src/my/project/pokeclone/map/HeroHome1F.java
@@ -0,0 +1,43 @@
+package my.project.pokeclone.map;
+
+import my.project.pokeclone.Handler;
+import my.project.pokeclone.entity.Player;
+import my.project.pokeclone.graphics.Asset;
+
+public class HeroHome1F extends GameMap{
+
+    public HeroHome1F(Handler handler, Player player) {
+        super(handler, player);
+    }
+
+    @Override
+    protected void init() {
+        bufferedImage = Asset.heroHome1F;
+        width = Asset.heroHome1F.getWidth();
+        height = Asset.heroHome1F.getHeight();
+        tileSide = 16;
+        super.init();
+        loadCollisionTiles("res/map tiles/Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town - Hero Home 1F - Tile Map.txt");
+        loadTeleportTiles("res/map tiles/Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town - Hero Home 1F - Teleport Map.txt");
+
+        entityManager.addEntity(player);
+
+    }
+
+    @Override
+    public GameMap getDestinationMap(int marker) {
+        GameMap destination = this;
+        if (marker == 2) destination = handler.getGameState().getPalletTown();
+        return destination;
+    }
+
+    @Override
+    public int[] getDestinationPosition(int marker) {
+        int[] position = {0, 0};
+        if (marker == 2) {
+            position[0] = 96;
+            position[1] = 110;
+        }
+        return position;
+    }
+}
