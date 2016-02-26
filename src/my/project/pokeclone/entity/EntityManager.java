55ee0488b598092fbd2ab7602baabbdc74b14d93
rm7740 <adavid8a@hotmail.com>
2016-02-25 22:41:10 -0500

    Add a Comparator to sort all entities by y position on EntityManager

--- src/my/project/pokeclone/entity/EntityManager.java
+++ src/my/project/pokeclone/entity/EntityManager.java
@@ -4,15 +4,23 @@ import my.project.pokeclone.Handler;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class EntityManager {
 
     private Handler handler;
     private ArrayList<Entity> entities;
+    private Comparator<Entity> sortRenderOrder;
 
     public EntityManager(Handler handler) {
         this.handler = handler;
-        entities = new ArrayList<Entity>();
+        entities = new ArrayList<>();
+        sortRenderOrder = (o1, o2) -> {
+            if(o1.getY() + o1.getHeight() < o2.getY() + o2.getHeight()){
+                return -1;
+            }
+            return 1;
+        };
     }
 
     public void addEntity(Entity entity) {
@@ -28,6 +36,7 @@ public class EntityManager {
             Entity entity = entities.get(i);
             entity.update();
         }
+        entities.sort(sortRenderOrder);
     }
 
     public void render(Graphics graphics){
